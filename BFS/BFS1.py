1caa9a030bcff89d4f0df23f5c4636ea5bb5536d
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2020-09-10 13:36:49 +0900

    Update BFS1.py

--- BFS/BFS1.py
+++ BFS/BFS1.py
@@ -1,35 +1,32 @@
 from collections import deque
 
-while True :
-  w, h = map(int,input().split())
-  if w == 0  and h == 0 : 
-    break
+while True : 
+    w,h = map(int,input().split())  
+    if w == 0 and h == 0 :  
+        break
+    graph = [list(map(int,input().split())) for _ in range(h)]  
 
-  graph = [list(map(int,input().split())) for _ in range(h)]
+    dx = [-1,1,0,0,1,1,-1,-1]   
+    dy = [0,0,1,-1,1,-1,1,-1]
 
-  dx = [-1,-1,0,1,1,1,0,-1]
-  dy = [0,1,1,1,0,-1,-1,-1]
+    queue = deque() 
+    result = 0  
+    for i in range(w) : 
+        for j in range(h) : 
+            if graph[i][j] == 1 :   
+                queue.append((i,j)) 
+                graph[i][j] = 2 
+                while queue : 
+                    x,y = queue.popleft()   
+                    for k in range(8) : 
+                        nx = x + dx[k]  
+                        ny = y + dy[k]
 
-  queue = deque()
-  count = 0 
-  for i in range(h) : 
-    for j in range(w) : 
-      if graph[i][j] == 1 :
-        queue.append((i,j))
-        graph[i][j] = 2
+                        if nx >= 0 and ny >= 0 and nx < h and ny < w :  
+                            if graph[nx][ny] == 1 : 
+                                queue.append((nx,ny))  
+                                graph[nx][ny] = 2   
+                    
+                else : result += 1  
 
-        while queue : 
-          x,y = queue.popleft()
-
-          for k in range(8) :
-            nx = x + dx[k]
-            ny = y + dy[k]
-
-            if 0 <= nx < h and 0 <= ny < w :
-              if graph[nx][ny] == 1 :
-                queue.append((nx,ny))
-                graph[nx][ny] = 2
-
-        else : 
-          count += 1
-  print(count)   
+    print(result) 
