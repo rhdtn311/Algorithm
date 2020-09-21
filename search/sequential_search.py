ce30a69d41a2dabc4baa1a4b3a970e78d746eae1
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2020-09-21 18:26:50 +0900

    Delete sequential_search.py

--- search/sequential_search.py
+++
@@ -1,6 +0,0 @@
-# 순차탐색
-#  n은 원소의 개수, target은 찾고자 하는 데이터, array는 target이 있는 리스트
-def sequential_search(n,target,array) : 
-    for i in range(n) : 
-        if array[i] == target : 
-            return i+1
