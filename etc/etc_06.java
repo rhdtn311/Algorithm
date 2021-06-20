7b0c961b4749123dfeb266dd558b6660b16c6701
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-06-20 20:51:14 +0900

    2021-06-20 프로그래머스 lv1 K번째 수

---
+++ etc/etc_06.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class etc_06 {
+    // 프로그래머스 level : K번째 수 (https://programmers.co.kr/learn/courses/30/lessons/42748)
+    public int[] solution(int[] array, int[][] commands) {
+
+        int[] result = new int[commands.length];
+        int index = 0;
+
+        for (int[] arr : commands) {
+
+            int i = arr[0];
+            int j = arr[1];
+            int k = arr[2];
+
+            ArrayList<Integer> arrayList = new ArrayList<>();
+
+            int count = 0;
+
+            for (int l : array) {
+                ++count;
+                if (count >= i && count <= j) {
+                    arrayList.add(l);
+                }
+            }
+
+            Collections.sort(arrayList);
+
+            result[index] = arrayList.get(k-1);
+            index += 1;
+
+        }
+        return result;
+    }
+}
