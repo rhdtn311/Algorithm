9c45a9cebcfc9ad4c1aa1e8b4c35b0550f61d72f
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-07-08 17:58:42 +0900

    21-07-08 ) 프로그래머스 lv 1 : 정수 제곱근 판별

---
+++ etc/etc_38.java
@@ -0,0 +1,8 @@
+public class etc_38 {
+    // 프로그래머스 lv1 : 정수 제곱근 판별 (https://programmers.co.kr/learn/courses/30/lessons/12934)
+    public long solution(long n) {
+        double value = Math.sqrt(n);
+        long result = n % value == 0 ? (long)Math.pow(value+1,2) : -1;
+        return result;
+    }
+}
