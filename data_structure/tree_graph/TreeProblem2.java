9a4618d0e8c39545343645155d01d986fd30bce1
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-07-23 17:21:29 +0900

    21-07-23 트리 문제 2

---
+++ data_structure/tree_graph/TreeProblem2.java
@@ -0,0 +1,36 @@
+package tree_graph;
+
+import java.util.*;
+
+// leetcode easy : 94. Binary Tree Inorder Traversal(https://leetcode.com/problems/binary-tree-inorder-traversal/)
+public class TreeProblem2 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> list = new ArrayList<>();
+        inOrder(root,list);
+        return list;
+
+    }
+
+    public void inOrder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+}
