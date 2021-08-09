b4b5049b8b9e7523b518e5c49e7cd9be58557e5a
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-08-09 17:21:25 +0900

    21/08/09 퀵 소트 수정

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -9,8 +9,8 @@ public class QuickSort {
         int right = end;
 
         while (left <= right) {
-            while (left <= end && array[left] < array[pivot]) left ++;
-            while (right > start && array[right] > array[pivot]) right --;
+            while (left <= end && array[left] <= array[pivot]) left ++;
+            while (right > start && array[right] >= array[pivot]) right --;
 
             if (left > right) {
                 swap(pivot, right, array);
@@ -27,5 +27,13 @@ public class QuickSort {
         int rightValue = array[right];
         array[left] = rightValue;
         array[right] = leftValue;
+    }
+
+    public static void main(String[] args) {
+        QuickSort quickSort = new QuickSort();
+        int [] array = new int[] {6,2,4,1,3,7,9,8,-1,5,1};
+        System.out.println(Arrays.toString(array));
+        quickSort.quickSort(0, array.length - 1, array);
+        System.out.println(Arrays.toString(array));
     }
 }
