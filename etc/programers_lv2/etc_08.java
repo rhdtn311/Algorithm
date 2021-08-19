017736d90e4690edfa7d9f457175694ca9b2e5ae
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-08-19 22:27:09 +0900

    21-08-19) 프로그래머스 lv2 : 행렬 테두리 회전하기

--- etc/programers_lv2/etc_08.java
+++ etc/programers_lv2/etc_08.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class etc_08 {
     public int[] solution(int rows, int columns, int[][] queries) {
 
+        // 행렬만들기
         int value = 1;
         int[][] matrix = new int[rows][columns];
         for (int i = 0; i < rows; i++) {
@@ -25,38 +26,52 @@ public class etc_08 {
         return result;
     }
 
+    // 행렬을 시계방향으로 회전
     public int rotateClockwise(int[][] matrix, int[] query) {
+        // 인덱스에 맞게 -1
         int startRow = query[0] - 1;
         int endRow = query[2] -1;
         int startColumn = query[1] -1;
         int endColumn = query[3] - 1;
 
+        // 행과 열을 1씩 움직이기 위한 배열
         int[] moveRow = new int[] {0,1,0,-1};
         int[] moveColumn = new int[] {1,0,-1,0};
 
+        // 왼쪽 위 부터 회전
         int nowRow = startRow;
         int nowColumn = startColumn;
 
+        // 모든 테두리 값을 저장할 list
         ArrayList<Integer> arrayList = new ArrayList<>();
+        // 이 전 값을 저장할 큐
         Queue<Integer> queue = new LinkedList<>();
+        // 큐에 테두리의 가장 마지막 값을 삽입해 놓음
         queue.add(matrix[startRow + 1][startColumn]);
+        // 반복문은 가장 오른쪽 부터 반복적으로 돌다가 테두리 밖으로 나갈 때 방향을 시계방향으로 바꾼다.
         for (int i = 0; i < 4;) {
 
             int nextRow = nowRow +  moveRow[i];
             int nextColumn = nowColumn + moveColumn[i];
 
+            // 다음 위치가 테두리를 넘어간다면 방향을 시계방향으로 회전하고(i++) continue
             if (nextRow > endRow || nextColumn > endColumn || nextRow < startRow || nextColumn < startColumn) {
                 i++;
                 continue;
             }
+            // arrayList에 현재 값을 넣음
             arrayList.add(matrix[nowRow][nowColumn]);
+            // queue에 현재 값을 넣음
             queue.add(matrix[nowRow][nowColumn]);
+            // 행렬의 현재 위치를 queue에 넣은 값으로 바꿈
             matrix[nowRow][nowColumn] = queue.poll();
 
+            // 현재 위치를 다음 위치로 변경
             nowRow = nextRow;
             nowColumn = nextColumn;
         }
 
+        // 정렬 후 가장 첫번째 값 (최솟값) 리턴
         Collections.sort(arrayList);
         return arrayList.get(0);
 
