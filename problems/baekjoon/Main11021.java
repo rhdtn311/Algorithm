6b65feb8522f5f9312cc8b6901a47a55c1dc59f2
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-09-13 01:28:10 +0900

    21-09-13) 백준 11021 : A+B -7

---
+++ problems/baekjoon/Main11021.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            String[] s = br.readLine().split(" ");
+            int i1 = Integer.parseInt(s[0]);
+            int i2 = Integer.parseInt(s[1]);
+            System.out.println("Case #" + i + ": " + (i1+i2));
+        }
+    }
+}
