bde36c9232907c4208449a373710f7f285195785
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-09-13 00:41:24 +0900

    21-09-13) 백준 2884 : 알람 시계

---
+++ problems/baekjoon/Main2884.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int hour = Integer.parseInt(s[0]);
+        int minute = Integer.parseInt(s[1]);
+
+        int newMinute = minute - 45;
+        if (newMinute < 0) {
+            if (hour > 0) {
+                hour -= 1;
+            } else {
+                hour = 23;
+            }
+            newMinute = 60 + newMinute;
+        }
+
+        System.out.println(hour + " " + newMinute);
+    }
+}
