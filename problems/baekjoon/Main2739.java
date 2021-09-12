74d0714a179e365f82e74bede0cce281cc02e638
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-09-13 00:46:43 +0900

    21-09-13) 백준 2739 : 구구단

---
+++ problems/baekjoon/Main2739.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2739 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(n + " * " + i + " = " + (n*i));
+        }
+    }
+}
