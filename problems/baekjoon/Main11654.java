202406cc25b12a5a12f32965e0f310e4bd4623a8
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-09-17 23:25:07 +0900

    21-09-17) 백준 11654 : 아스키 코드

---
+++ problems/baekjoon/Main11654.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        char c = s.charAt(0);
+
+        System.out.println((int)c);
+    }
+}
