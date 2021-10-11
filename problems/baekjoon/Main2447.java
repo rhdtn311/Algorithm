a0a8aca3de46da224900ffdec04e430f702833d9
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-10-11 13:30:26 +0900

    2021-10-11) 백준 2447 : 별 찍기 - 10

---
+++ problems/baekjoon/Main2447.java
@@ -0,0 +1,58 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main2447 {
+    static char[][] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new char[N][N];
+
+        makeStar(0, 0, N, false);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+
+
+    }
+
+    private static void makeStar(int x, int y, int N, boolean isBlank) {
+        if (isBlank) {
+            for (int i = x; i < x + N; i++) {
+                for (int j = y; j < y + N; j++) {
+                    arr[i][j] = ' ';
+                }
+            }
+            return;
+        }
+
+        if (N == 1) {
+            arr[x][y] = '*';
+            return;
+        }
+
+        int size = N / 3;
+        int count = 0;
+        for (int i = x; i < x + N; i += size) {
+            for (int j = y; j < y + N; j += size) {
+                count++;
+                if (count == 5) {
+                    makeStar(i, j, size, true);
+                } else {
+                    makeStar(i, j, size, false);
+                }
+            }
+        }
+    }
+}
