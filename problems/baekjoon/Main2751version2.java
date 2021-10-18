5516658e4230532b4e55a3597b3af1f47f79ceb0
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-10-18 10:16:35 +0900

    21-10-18) 백준 2751 : 수 정렬하기2(힙 정렬)

---
+++ problems/baekjoon/Main2751version2.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main2751version2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(pq.poll()).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
