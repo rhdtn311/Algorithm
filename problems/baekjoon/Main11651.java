5dea0d9fc6857a9a7004670e547d0afa9afa1a13
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-10-19 19:49:44 +0900

    21-10-19) 백준 11651 : 좌표 정렬하2(Arrays.sort)

---
+++ problems/baekjoon/Main11651.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            int x = Integer.parseInt(s.split(" ")[0]);
+            int y = Integer.parseInt(s.split(" ")[1]);
+
+            arr[i][0] = x;
+            arr[i][1] = y;
+        }
+
+        Comparator<int[]> comparator = new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                } else {
+                    return o1[1] - o2[1];
+                }
+            }
+        };
+
+        Arrays.sort(arr, comparator);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
