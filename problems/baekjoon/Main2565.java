4926257b67243e87185ac72ec46b9c3c51a3d7fe
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-10-31 13:45:37 +0900

    21-10-30) 백준 11053 : 전깃줄

---
+++ problems/baekjoon/Main2565.java
@@ -0,0 +1,50 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main2565 {
+
+    static int[][] arr;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n];
+
+        arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(s[0]);
+            arr[i][1] = Integer.parseInt(s[1]);
+            dp[i] = 1;
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i][1] > arr[j][1]) {
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(n - max);
+    }
+}
