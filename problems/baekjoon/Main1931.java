a7e446dac0754ea4c35df2fe0bbebd68f1f049f4
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-11-03 01:38:14 +0900

    21-11-03) 백준 1931. 회의실 배정

---
+++ problems/baekjoon/Main1931.java
@@ -0,0 +1,48 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main1931 {
+
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(s[0]);
+            arr[i][1] = Integer.parseInt(s[1]);
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                } else {
+                    return o1[1] - o2[1];
+                }
+            }
+        });
+
+        int prev = arr[0][1];
+        int count = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i][0] >= prev) {
+                prev = arr[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(Arrays.deepToString(arr));
+        System.out.println(count);
+
+    }
+}
