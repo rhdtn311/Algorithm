71c3ed8ab36cd9c73898b71b407dfc5ade6432ea
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-11-21 18:20:40 +0900

    21-11-21) 백준 2606 : 바이러스 (dfs)

---
+++ problems/baekjoon/Main2606.java
@@ -0,0 +1,79 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main2606 {
+
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int computers = Integer.parseInt(br.readLine());
+        int edge = Integer.parseInt(br.readLine());
+
+        visited = new boolean[computers + 1];
+
+        for (int i = 0; i <= computers; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edge; i++) {
+            String[] input = br.readLine().split(" ");
+
+            int start = Integer.parseInt(input[0]);
+            int end = Integer.parseInt(input[1]);
+
+            graph.get(start).add(end);
+            graph.get(end).add(start);
+        }
+
+//        dfs(1);
+
+        bfs(1);
+        int count = 0;
+        for (int i = 0; i < visited.length; i++) {
+            if (visited[i]) {
+                count++;
+            }
+        }
+
+        System.out.println(count - 1);
+    }
+
+    static void dfs(int start) {
+
+        visited[start] = true;
+
+        for (int i = 0; i < graph.get(start).size(); i++) {
+            if (!visited[graph.get(start).get(i)]) {
+                dfs(graph.get(start).get(i));
+            }
+        }
+    }
+
+    static void bfs(int start) {
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            for (int i = 0; i < graph.get(now).size(); i++) {
+                if (!visited[graph.get(now).get(i)]) {
+                    queue.add(graph.get(now).get(i));
+                    visited[graph.get(now).get(i)] = true;
+                }
+            }
+        }
+    }
+}
