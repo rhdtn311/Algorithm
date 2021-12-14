f4c5d2f0d8c7c68bc181349361e4d94fa1f7af32
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-12-14 22:05:47 +0900

    21-12-14) 프로그래머스 lv1 : 직사각형 별찍기

---
+++ problems/programers_lv1/Solution53.java
@@ -0,0 +1,21 @@
+package programers_lv1;
+
+import java.util.Scanner;
+
+public class Solution53 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
