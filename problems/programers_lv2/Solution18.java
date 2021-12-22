12858d86cd37ace3bb73f5546adaa45a2f324c06
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-12-22 23:39:15 +0900

    21-12-22) 프로그래머스 lv2 : 전화번호 목록

---
+++ problems/programers_lv2/Solution18.java
@@ -0,0 +1,17 @@
+package programers_lv2;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Solution18 {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
