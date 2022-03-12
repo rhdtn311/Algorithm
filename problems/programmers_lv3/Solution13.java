a5b402996b4fb2f9c85c40d05d55918baf93e170
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2022-03-13 03:27:06 +0900

    22-03-12) 프로그래머스 lv3 :  등굣길

--- problems/programmers_lv3/Solution13.java
+++ problems/programmers_lv3/Solution13.java
@@ -4,51 +4,35 @@ import java.util.Arrays;
 
 public class Solution13 {
 
-    static long[][] dp;
     static int[][] arr;
-
-    public static void main(String[] args) {
-        Solution13 solution13 = new Solution13();
-        int solution = solution13.solution(4, 3, new int[][]{{2, 2}});
-        System.out.println(solution);
-    }
-
     public int solution(int m, int n, int[][] puddles) {
         arr = new int[n][m];
-        dp = new long[n][m];
 
         for (int[] puddle : puddles) {
             arr[puddle[1] - 1][puddle[0] - 1] = -1;
         }
 
-        for (int i = 0; i < m; i++) {
-            if (arr[0][i] == -1) {
-                break;
-            }
-            dp[0][i] = 1;
-        }
-
+        arr[0][0] = 1;
         for (int i = 0; i < n; i++) {
-            if (arr[i][0] == -1) {
-                break;
-            }
-            dp[i][0] = 1;
-        }
-
-
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (arr[i][j] == -1) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0) {
                     continue;
                 }
 
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
+                if (arr[i][j] == -1) {
+                    arr[i][j] = 0;
+                    continue;
+                }
+                if (i == 0) {
+                    arr[i][j] = arr[i][j - 1] % 1000000007;
+                } else if (j == 0) {
+                    arr[i][j] = arr[i - 1][j] % 1000000007;
+                } else {
+                    arr[i][j] = (arr[i - 1][j] + arr[i][j - 1]) % 1000000007;
+                }
             }
         }
 
-        System.out.println(Arrays.deepToString(dp));
-
-        return (int)dp[n - 1][m - 1] % 1000000007;
+        return arr[n - 1][m - 1] % 1000000007;
     }
 }
