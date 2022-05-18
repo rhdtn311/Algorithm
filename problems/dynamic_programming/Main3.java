b948bae87cf3e1b9e3be8704b41d5e736b81a29c
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2022-05-18 14:57:52 +0900

    22-05-18) dp 3 : 최대부분증가수열(LIS)

---
+++ problems/dynamic_programming/Main3.java
@@ -0,0 +1,27 @@
+package dynamic_programming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
